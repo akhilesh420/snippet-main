2e597a6964b2a01f8700d5d8bac9a52cbb637828
akhilesh420 <72787785+akhilesh420@users.noreply.github.com>
2020-11-04 23:34:42 +0530

    merged with akhilesh

--- src/app/shared/miscellaneous.service.ts
+++ src/app/shared/miscellaneous.service.ts
@@ -5,26 +5,5 @@ import { Subject } from 'rxjs';
   providedIn: 'root'
 })
 export class MiscellaneousService {
-<<<<<<< HEAD
-
-  private loadingBar = new Subject<string>();
-
   constructor() { }
-  setLoading(value: number) { //percentage value
-
-    value = value > 100 ? 100 : value; //cap value at 100%
-
-    this.loadingBar.next(value.toString() + '%');
-
-    if (value === 100) {
-      setTimeout(func => this.loadingBar.next('0%'), 500)
-    }
-  }
-
-  getLoading() {
-    return this.loadingBar;
-  }
-=======
-  constructor() { }
->>>>>>> akhilesh
 }
