13c109d5e6d8256f3f7a9c08e8face39b2818a2a
akhilesh420 <72787785+akhilesh420@users.noreply.github.com>
2020-12-16 18:28:45 +0530

    optimizing

--- functions/index.js
+++ functions/index.js
@@ -6,14 +6,15 @@ const os = require('os');
 const fs = require('fs');
 const ffmpeg = require('fluent-ffmpeg');
 const ffmpeg_static = require('ffmpeg-static');
-const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path
+const spawn = require('child-process-promise').spawn;
+const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
 
 const runtimeOpts = {
   timeoutSeconds: 540,
   memory: '2GB'
 }
 
-exports.postCreate = functions.runWith(runtimeOpts).firestore
+exports.contentCreate = functions.runWith(runtimeOpts).firestore
   .document('post content/{pid}')
   .onCreate(async (snap, context) => {
 
@@ -24,49 +25,134 @@ exports.postCreate = functions.runWith(runtimeOpts).firestore
     const contentType = newValue.fileFormat;
 
     functions.logger.info('File name:', fileName);
-
-    if (!contentType.includes('video')) {
-      functions.logger.info('File skipped because file type:', contentType);
-      return null;
-    }
+    functions.logger.info('File type:', contentType);
 
     const filePath = 'Post/' + fileName;
     const bucket = admin.storage().bucket(fileBucket);
     const tempFilePath = path.join(os.tmpdir(), fileName);
-    const outputFilePath =  path.join(os.tmpdir(), 'faststarted.mp4');
 
-    await bucket.file(filePath).download({destination: tempFilePath})
+    await bucket.file(filePath).download({destination: tempFilePath});
 
     functions.logger.info('File downloaded locally to', tempFilePath);
 
-    await faststart(tempFilePath, outputFilePath, bucket, filePath);
+    if (contentType.startsWith('image/gif')) {
+      const outputFilePath =  path.join(os.tmpdir(), 'optimized.gif');
+      await gif_processing(bucket, outputFilePath, tempFilePath, filePath);
+    } else if (contentType.startsWith('image/')) {
+      const outputFilePath =  path.join(os.tmpdir(), 'compressed.jpeg');
+      await image_processing(bucket, outputFilePath, tempFilePath, filePath);
+    } else if (contentType.includes('video/')) {
+      const outputFilePath =  path.join(os.tmpdir(), 'faststarted.mp4');
+      await faststart(tempFilePath, outputFilePath, bucket, filePath);
+    }
   });
 
-  function faststart(tempFilePath, outputFilePath, bucket, filePath) {
-    return new Promise((resolve,reject) => {
-      ffmpeg(tempFilePath)
-      .setFfmpegPath(ffmpegPath)
-      .outputOptions(['-movflags +faststart'])
-      .output(outputFilePath)
-      .on('error', (err, stdout, stderr) => {
-        functions.logger.info('An error occurred', err, stdout, stderr);
-        return fs.unlinkSync(tempFilePath);
-      })
-      .on('start', (commandLine) => functions.logger.info('started video processing: ', commandLine))
-      .on('end', (filenames) => {
-        functions.logger.info('finished video processing: ', filenames);
+exports.stickerCreate = functions.runWith(runtimeOpts).firestore
+.document('sticker content/{pid}')
+.onCreate(async (snap, context) => {
 
-        bucket.upload(outputFilePath, {
-          destination: filePath
-        })
-        .then(() => {
-          functions.logger.info('Successfully uploaded to storage');
-          })
-        .catch((error) => functions.logger.info('Failed to upload to storage', error))
-        .finally(() => {
-          functions.logger.info('Finished execution');
-          return fs.unlinkSync(tempFilePath);});
+  const fileBucket = 'snippet-test-716cd.appspot.com';
+
+  const newValue = snap.data();
+  const fileName = newValue.name;
+  const contentType = newValue.fileFormat;
+
+  functions.logger.info('File name:', fileName);
+  functions.logger.info('File type:', contentType);
+
+  const filePath = 'Post/' + fileName;
+  const bucket = admin.storage().bucket(fileBucket);
+  const tempFilePath = path.join(os.tmpdir(), fileName);
+
+  await bucket.file(filePath).download({destination: tempFilePath});
+
+  functions.logger.info('File downloaded locally to', tempFilePath);
+
+  if (contentType.startsWith('image/gif')) {
+    const outputFilePath =  path.join(os.tmpdir(), 'optimized.gif');
+    await gif_processing(bucket, outputFilePath, tempFilePath, filePath);
+  } else if (contentType.startsWith('image/')) {
+    const outputFilePath =  path.join(os.tmpdir(), 'compressed.jpeg');
+    await image_processing(bucket, outputFilePath, tempFilePath, filePath);
+  }
+});
+
+exports.dpCreate = functions.runWith(runtimeOpts).firestore
+.document('display picture/{uid}')
+.onCreate(async (snap, context) => {
+
+  const fileBucket = 'snippet-test-716cd.appspot.com';
+
+  const newValue = snap.data();
+  const fileName = newValue.name;
+  const contentType = newValue.fileFormat;
+
+  functions.logger.info('File name:', fileName);
+  functions.logger.info('File type:', contentType);
+
+  const filePath = 'Post/' + fileName;
+  const bucket = admin.storage().bucket(fileBucket);
+  const tempFilePath = path.join(os.tmpdir(), fileName);
+
+  await bucket.file(filePath).download({destination: tempFilePath});
+
+  functions.logger.info('File downloaded locally to', tempFilePath);
+
+  if (contentType.startsWith('image/')) {
+    const outputFilePath =  path.join(os.tmpdir(), 'compressed.jpeg');
+    await image_processing(bucket, outputFilePath, tempFilePath, filePath);
+  }
+});
+
+function faststart(tempFilePath, outputFilePath, bucket, filePath) {
+  return new Promise((resolve, reject) => {
+    ffmpeg(tempFilePath)
+    .setFfmpegPath(ffmpegPath)
+    .outputOptions(['-movflags +faststart'])
+    .output(outputFilePath)
+    .on('error', (err, stdout, stderr) => {
+      functions.logger.info('An error occurred', err, stdout, stderr);
+      fs.unlinkSync(outputFilePath);
+      return fs.unlinkSync(tempFilePath);
+    })
+    .on('start', (commandLine) => functions.logger.info('started video processing for faststart: ', commandLine))
+    .on('end', async (filenames) => {
+      functions.logger.info('finished video processing: ', filenames);
+      await uploadToStorage(bucket, outputFilePath, tempFilePath, filePath);
+    })
+    .run();
+  });
+};
+
+function image_processing(bucket, outputFilePath, tempFilePath, filePath) {
+  return new Promise( async (resolve, reject) => {
+    functions.logger.info('started image processing for interlace');
+    await spawn('convert', ['-sampling-factor','4:2:0', '-strip', '-quality', '85', '-interlace', 'Plane', '-gaussian-blur', '0.05', '-colorspace', 'RGB', tempFilePath, outputFilePath]);
+    await uploadToStorage(bucket, outputFilePath, tempFilePath, filePath);
+  });
+}
+
+function gif_processing(bucket, outputFilePath, tempFilePath, filePath) {
+  return new Promise( async (resolve, reject) => {
+    functions.logger.info('started image processing for interlace');
+    await spawn('convert', ['-sampling-factor','4:2:0', '-strip', '-quality', '85', '-interlace', 'Plane', tempFilePath, outputFilePath]);
+    await uploadToStorage(bucket, outputFilePath, tempFilePath, filePath);
+  });
+}
+
+function uploadToStorage(bucket, outputFilePath, tempFilePath, filePath) {
+  return new Promise((resolve, reject) => {
+    bucket.upload(outputFilePath, {
+      destination: filePath
+    })
+    .then(() => {
+      functions.logger.info('Successfully uploaded to storage');
       })
-      .run();
+    .catch((error) => functions.logger.info('Failed to upload to storage', error))
+    .finally(() => {
+      functions.logger.info('Finished execution');
+      fs.unlinkSync(outputFilePath);
+      return fs.unlinkSync(tempFilePath);
     });
-  };
+  });
+}
