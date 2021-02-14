61b85f9b989d5de601af2481e8244866d5f13bc2
akhilesh420 <72787785+akhilesh420@users.noreply.github.com>
2021-02-14 17:00:45 +0530

    css variables made

--- src/app/shared/window.service.ts
+++ src/app/shared/window.service.ts
@@ -7,6 +7,13 @@ import { BehaviorSubject} from "rxjs";
 export class WindowStateService {
 
   public screenWidthValue = new BehaviorSubject<number>(null);
+  private designDeskWidth = 1440;
+  private designDeskHeight =  900;
+  private designMobileWidth = 375;
+  private designMobileHeight = 640;
+  private tabletCheck: boolean;
+  private mobileCheck: boolean;
+  private height: number;
 
   constructor() {
     this.checkWidth();
@@ -14,7 +21,21 @@ export class WindowStateService {
 
   public checkWidth() {
     var width = window.innerWidth;
+    this.height = window.innerHeight;
     this.screenWidthValue.next(width);
+
+    width < 800 ? this.tabletCheck = true : this.tabletCheck = false;
+    width < 550 ? this.mobileCheck = true : this.mobileCheck = false;
+
+    document.documentElement.style.setProperty('--desk-x', (width/this.designDeskWidth).toString());
+    document.documentElement.style.setProperty('--mobile-x', (width/this.designMobileWidth).toString());
+
+    if (!this.tabletCheck) this.setHeights();
+  }
+
+  public setHeights() {
+    document.documentElement.style.setProperty('--desk-y', (this.height/this.designDeskHeight).toString());
+    document.documentElement.style.setProperty('--mobile-y', (this.height/this.designMobileHeight).toString());
   }
 
 }
